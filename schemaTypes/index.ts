b506ae2548e52b558e53f8e46a2fadbc625f89a5
brob <bryanlrobinson@gmail.com>
2025-11-22 11:07:22 -0500

    Adds box to list

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,5 @@
 import Material from './Material'
 import MaterialCategory from './MaterialCategory'
 import Program from './Program'
-
-export const schemaTypes = [Material, MaterialCategory, Program]
+import Box from './Box'
+export const schemaTypes = [Material, MaterialCategory, Program, Box]
